0fd74f6053e2b8b2902acb21ed81b3e904583f21
Philipp Klaus <philipp.klaus@gmail.com>
2012-11-13 15:16:38 +0100

    Adding debug parameter and custom Exceptions

--- Leybold.py
+++ Leybold.py
@@ -7,10 +7,11 @@ class ItrUpdater(object):
     fixed_bytes = {0: chr(0x7), 1: chr(0x5) }
     checksum_byte = 8
     byte_num = 9
-    def __init__(self, callback):
+    def __init__(self, callback, debug=False):
         self.buf = ""
         self.in_sync = False
         self.callback = callback
+        self.debug = debug
 
     def add_new_input(self, data):
         self.buf += data
@@ -39,6 +40,7 @@ class ItrUpdater(object):
             if self.buf[bytenum] != self.fixed_bytes[bytenum]:
                 return False
         if self.buf[self.checksum_byte] != chr(self.checksum256(self.buf[1:8])):
+            if self.debug: print 'problematic message %s' % repr(self.buf[:9])
             return False
         buf = self.buf[0:9]
         self.buf = self.buf[9:]
@@ -56,19 +58,36 @@ class ItrUpdater(object):
 
 class ITR(object):
     pressure = 1.0
+    def __init__(self, debug = False):
+        self.debug = debug
     def parse_status(self, status):
         #self.check_message(status)
-        #self.parse_state(status[2])
-        #self.parse_error(status[3])
-        self.parse_pressure(status[4:6])
-        #self.parse_version(status[6])
-        #self.parse_type(status[7])
-        print "%.3E" % self.pressure
+        try:
+            #self.parse_state(status[2])
+            #self.parse_error(status[3])
+            self.parse_pressure(status[4:6])
+            #self.parse_version(status[6])
+            #self.parse_type(status[7])
+        except e:
+            if self.debug: print str(e)
+            raise ParseError(e)
+        #self.last_update = time.time()
+        #print "%.3E" % self.pressure
 
     def parse_pressure(self, data):
         self.pressure = 10.**((ord(data[0])*256+ord(data[1]))/4000.-12.5)
 
 class ITR90(ITR):
+    pass
+
+
+class LeyboldError(Exception):
+    pass
+
+class VacuumGaugeError(LeyboldError):
+    pass
+
+class ParseError(VacuumGaugeError):
     pass
 
 toHex = lambda x:"".join([hex(ord(c))[2:].zfill(2) for c in x])
@@ -82,10 +101,10 @@ if __name__ == "__main__":
     s1 = SerialReceiver(device)
     s1.start()
 
-    itr90 = ITR90()
+    itr90 = ITR90(debug = True)
 
     #iu = ItrUpdater(printer)
-    iu = ItrUpdater(itr90.parse_status)
+    iu = ItrUpdater(itr90.parse_status, debug=True)
 
     try:
         while True:
