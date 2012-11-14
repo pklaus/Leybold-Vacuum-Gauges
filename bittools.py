0d84296bf781f1d9f86278721b51fd8dd5b345be
Philipp Klaus <philipp.klaus@gmail.com>
2012-11-14 11:08:29 +0100

    Changing to my personal conventions of documentation and naming

--- bittools.py
+++ bittools.py
@@ -1,26 +1,22 @@
 
 """ More to be found on http://wiki.python.org/moin/BitManipulation . """
 
-# testBit() returns a nonzero result, 2**offset, if the bit at 'offset' is one.
-
-def testBit(int_type, offset):
-    mask = 1 << offset
+def test_bit(int_type, bit_offset):
+    """ returns a nonzero result, 2**bit_offset, if the bit at 'bit_offset' is one. """
+    mask = 1 << bit_offset
     return(int_type & mask)
 
-# setBit() returns an integer with the bit at 'offset' set to 1.
-
-def setBit(int_type, offset):
-    mask = 1 << offset
+def set_bit(int_type, bit_offset):
+    """ returns an integer with the bit at 'bit_offset' set to 1. """
+    mask = 1 << bit_offset
     return(int_type | mask)
 
-# clearBit() returns an integer with the bit at 'offset' cleared.
-
-def clearBit(int_type, offset):
-    mask = ~(1 << offset)
+def clear_bit(int_type, bit_offset):
+    """ returns an integer with the bit at 'bit_offset' cleared. """
+    mask = ~(1 << bit_offset)
     return(int_type & mask)
 
-# toggleBit() returns an integer with the bit at 'offset' inverted, 0 -> 1 and 1 -> 0.
-
-def toggleBit(int_type, offset):
-    mask = 1 << offset
+def toggle_bit(int_type, bit_offset):
+    """ returns an integer with the bit at 'bit_offset' inverted, 0 -> 1 and 1 -> 0. """
+    mask = 1 << bit_offset
     return(int_type ^ mask)
