867ee01a2494dc0bc36cf0704e77fbb9077d32b3
Philipp Klaus <philipp.klaus@gmail.com>
2013-05-16 15:42:09 +0200

    Adding a web interface to the web API.
    Plan for the future: The API should be mounted into this web app, so that
    you can either start the API alone or the Interface+API.

---
+++ interfaceserver.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+from bottle import Bottle, static_file, TEMPLATE_PATH
+from bottle import jinja2_view as view
+
+interface = Bottle()
+
+STATIC_ROOT = os.path.join(os.path.split(os.path.realpath(__file__))[0],'./webresources/static/')
+TEMPLATE_PATH.append(os.path.join(os.path.split(os.path.realpath(__file__))[0],'./webresources/views/'))
+
+@interface.route('/')
+@view('home.jinja2')
+def index_page():
+    return dict()
+
+@interface.route('/digital-display')
+@view('digital-display.jinja2')
+def entire_history():
+    return dict()
+
+@interface.route('/static/<path:path>')
+def static(path):
+    return static_file(path, root=STATIC_ROOT)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Start the Leybold Vacuum Gauges Interface Web-Server')
+    parser.add_argument('-p', '--port', default='8090', help='The port to run the web server on.')
+    parser.add_argument('-6', '--ipv6', action='store_true', help='Listen to incoming connections via IPv6 instead of IPv4.')
+    parser.add_argument('-d', '--debug', action='store_true', help='Start in debug mode (with verbose HTTP error pages.')
+    args = parser.parse_args()
+
+    if args.debug and args.ipv6:
+        args.error('You cannot use IPv6 in debug mode, sorry.')
+
+    if args.debug:
+        interface.run(host='0.0.0.0', port=args.port, debug=True, reloader=True)
+    else:
+        if args.ipv6:
+            # CherryPy is Python3 ready and has IPv6 support:
+            interface.run(host='::', server='cherrypy', port=args.port)
+        else:
+            interface.run(host='0.0.0.0', server='cherrypy', port=args.port)
---
+++ webresources/static/js/digital-display.js
@@ -0,0 +1,47 @@
+var slug = function(str) {
+    var $slug = '';
+    var trimmed = $.trim(str);
+    $slug = trimmed.replace(/[^a-z0-9-]/gi, '-').
+    replace(/-+/g, '-').
+    replace(/^-|-$/g, '');
+    return $slug.toLowerCase();
+}
+
+$.ajax({
+    url:      'http://localhost:8080/gauges',
+    method:   'GET',
+    dataType: 'json',
+    timeout:  1000,
+    success:  function(data) {
+        var gauges = data.gauges;
+        for (var key in gauges) {
+            var name = gauges[key];
+            $('#gauge_container').empty()
+            $('<div>', { id: slug(name), class: 'gauge' })
+                    .append( $('<div>', { class: 'label', text: name }))
+                    .append( $('<div>', { class: 'value' }))
+                    .appendTo('#gauge_container');
+        }
+    },
+    error: function(xhr, textStatus, errorThrown){
+        $('#gauge_container').empty()
+        $('<div>', { class: 'note error', text: 'Sorry, could not connect to API server.' }).appendTo('#gauge_container');
+    }
+});
+
+var tv = 500;
+var iv = setInterval( function() {
+    $.ajax({
+        url:      'http://localhost:8080/pressure/all',
+        method:   'GET',
+        dataType: 'json',
+        timeout:  1000,
+        success:  function(pressure) {
+            for (var key in pressure) {
+                $('#'+slug(key)).children('.value').html(pressure[key]['pressure'].toExponential(3).replace(/e/g, 'E'));
+            }
+        },
+        error: function(xhr, textStatus, errorThrown){
+        }
+    });
+}, tv );
